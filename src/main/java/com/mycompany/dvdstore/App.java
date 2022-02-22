2559349c24ca0aaba90adf10a5e46b27a2b1725a
diallo <souleymane.diallo@etudiant-univ-rennes1.fr>
2022-02-22 15:57:21 +0100

    Section 3 / Exercice 12 : DVDStore : Déplacer les annotations vers la classe exécutable

--- src/main/java/com/mycompany/dvdstore/App.java
+++ src/main/java/com/mycompany/dvdstore/App.java
@@ -2,7 +2,7 @@ package com.mycompany.dvdstore;
 
 import com.mycompany.dvdstore.controller.MovieController;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.*;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 
@@ -10,12 +10,16 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  * Hello world!
  *
  */
+@Configuration
+@ComponentScan(basePackages = {"com.mycompany.dvdstore.repository.file","com.mycompany.dvdstore.controller","com.mycompany.dvdstore.service"})
+@PropertySource("classpath:application.properties")
+@ImportResource("classpath:applicationContext.xml")
 public class App 
 {
     public static void main( String[] args )
     {
         //ApplicationContext context=new ClassPathXmlApplicationContext("applicationContext.xml");
-        ApplicationContext context=new AnnotationConfigApplicationContext(AppConfig.class);
+        ApplicationContext context=new AnnotationConfigApplicationContext(App.class);
         MovieController movieController=context.getBean(MovieController.class);
         movieController.addUsingConsole();
 
