fba3992ac32e2749fbd71f092738d88d78cf5560
diallo <souleymane.diallo@etudiant-univ-rennes1.fr>
2022-02-27 16:34:29 +0100

    Section 4 / Exercice 14 : DVDStore : Passer à Spring Boot

--- src/main/java/com/mycompany/dvdstore/App.java
+++ src/main/java/com/mycompany/dvdstore/App.java
@@ -1,25 +1,24 @@
 package com.mycompany.dvdstore;
 
 import com.mycompany.dvdstore.controller.MovieController;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.*;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 
 
 /**
  * Hello world!
  *
  */
-@Configuration
-@ComponentScan(basePackages = {"com.mycompany.dvdstore.repository.file","com.mycompany.dvdstore.controller","com.mycompany.dvdstore.service"})
-@PropertySource("classpath:application.properties")
-@ImportResource("classpath:applicationContext.xml")
+@SpringBootApplication
 public class App 
 {
     public static void main( String[] args )
     {
         //ApplicationContext context=new ClassPathXmlApplicationContext("applicationContext.xml");
-        ApplicationContext context=new AnnotationConfigApplicationContext(App.class);
+        //ApplicationContext context=new AnnotationConfigApplicationContext(App.class);
+        ApplicationContext context = SpringApplication.run(App.class);
         MovieController movieController=context.getBean(MovieController.class);
         movieController.addUsingConsole();
 
